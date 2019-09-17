e47ebd17c893b7594433d9a8b256164d85f0d426
LeftCircle <LeftCircle@users.noreply.github.com>
2019-09-17 16:42:04 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -12,22 +12,24 @@ def main():
     builder(size)
 
 # prints out the blocks according to a given height
+
+
 def builder(size):
     # Printing from the top down
-    for i in range(1 , size + 1):
+    for i in range(1, size + 1):
         # Prints 2 * height + 2 times
         spaces = size - i
 
         for i in range(spaces):
             print(" ", end="")
 
-        #now print blocks
+        # Now print blocks
         blocks = size - spaces
 
         for i in range(blocks):
             print('#', end="")
 
-        #now print two middle spaces
+        # Now print two middle spaces
         for i in range(2):
             print(' ', end="")
 
@@ -36,19 +38,16 @@ def builder(size):
         for i in range(blocks):
             print('#', end="")
 
-        #now we need a newline
         print()
 
-def getSize():
-    test = 1
-    while test == 1:
-        size = get_int("Give me an int between 1 and 8 inclusive")
 
-        if size < 1 or size > 8:
-            size = get_int("Give me an int between 1 and 8 inclusive")
-        else:
-            test = 0
-            return size
+def getSize():
+    size = 0
+    while size < 1 or size > 8:
+
+        size = get_int("Give me an int between 1 and 8 inclusive  ")
+
+    return size
 
 
 main()
